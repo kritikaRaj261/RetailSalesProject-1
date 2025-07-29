c920177901f0a897bfc835eaad9b2933cb6493a0
kritika Raj <69498411+kritikaRaj261@users.noreply.github.com>
2025-07-29 12:28:37 +0530

    Add files via upload

--- RetailSalesAnalysis_Project_1.sql
+++ RetailSalesAnalysis_Project_1.sql
@@ -1,17 +1,17 @@
 use  retail
 Create table Retail_Salesnew
                (
-		transactions_id INT PRIMARY KEY,
+				transactions_id INT PRIMARY KEY,
                 sale_date	DATE,
                 sale_time	TIME,
                 customer_id	 INT,
                 gender VARCHAR(25),
                 age	INT,
                 category VARCHAR(30),	
-		quantiy	INT,
+				quantiy	INT,
                 price_per_unit	INT,
                 cogs	INT,
-		total_sale INT
+				total_sale INT
 
                );
 SELECT * FROM retail_salesnew        
@@ -78,4 +78,48 @@ where category='Beauty'
 select * 
 from retail_salesnew
 where total_sale>1000;
+
+-- 6.Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.:
+select gender,Category ,count(transactions_id) as Total_Transaction
+from retail_salesnew
+group by gender,Category
+order by  2
+
+-- 7.Write a SQL query to calculate the average sale for each month. Find out best selling month in each year:
+
+with CT_AvgSale as (
+select Month(sale_date) as Month,year(sale_date) as year ,AVG(total_sale) as Avg_sales,
+RANK() OVER (PARTITION BY YEAR(sale_date) ORDER BY AVG(total_sale) DESC) AS rk
+from retail_salesnew
+group by Month(sale_date),year(sale_date)
+order by Month(sale_date) asc
+)
+select year,Month,Avg_sales
+from CT_AvgSale
+where rk=1
        
+ -- 8.**Write a SQL query to find the top 5 customers based on the highest total sales **:
+ 
+ select customer_id,sum(total_sale) as totalSales
+from retail_salesnew
+ group by customer_id
+ order by sum(total_sale) desc
+ Limit 5
+ 
+ -- 9. Write a SQL query to find the number of unique customers who purchased items from each category.:
+ select category,count(distinct customer_id) as customer_count
+ from retail_salesnew
+ group by category
+ 
+ -- 10.Write a SQL query to create each shift and number of orders (Example Morning <12, Afternoon Between 12 & 17, Evening >17):
+ 
+SELECT 
+  CASE 
+    WHEN HOUR(sale_time) < 12 THEN 'Morning'
+    WHEN HOUR(sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+    ELSE 'Evening'
+  END AS Shift,
+  count(transactions_id) as orderCount 
+FROM retail_salesnew
+group by shift
+
